1cbfe341eb64a8e041dde70e01a3962e6fae87bb
zeusK2 <kvnklg@gmail>
2016-05-26 18:05:58 -0500

    adding comments

--- src/main/java/com/example/CustomerController.java
+++ src/main/java/com/example/CustomerController.java
@@ -11,19 +11,23 @@ public class CustomerController {
     HashMap<String, Customer> customerMap = new HashMap<String, Customer>();
     private long id = 1;
 
+    //to get all populated customers
     @RequestMapping("/customer")
     public List<Customer> getAllCustomers() {
         return new ArrayList<>(customerMap.values());
     }
+    //insert new customer to hashmap
     @RequestMapping(value = "/customer", method = RequestMethod.POST)
     public void insertCustomer(@RequestBody Customer customer) {
         customer.setId(id);
         customerMap.put(String.valueOf(id++), customer);
     }
+    //remove customer according to id pathVariable
     @RequestMapping(value ="/customer/{id}", method = RequestMethod.DELETE)
     public void deleteCustomer(@PathVariable String id) {
         customerMap.remove(id);
     }
+    //get customer according to id pathVariable
     @RequestMapping(value="/customer/{id}")
     public Customer getCustomer(@PathVariable String id) {
         return customerMap.get(id);
